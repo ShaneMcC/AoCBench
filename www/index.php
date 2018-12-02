583e46a6a206c00e6e866bea2e70a4e6ef818598
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-02 17:29:03 +0000

    Basic www output of results.

---
+++ www/index.php
@@ -0,0 +1,49 @@
+<?php
+	$json = __DIR__ . '/../results.json';
+
+	if (!file_exists($json)) { die('No results yet.'); }
+
+	$data = json_decode(file_get_contents($json), true);
+
+	// Dump a table of benchmark results.
+
+	$particpants = array_keys($data['results']);
+
+	echo '<h2>Results</h2>', "\n";
+	echo '<table>';
+	// Participants
+	echo '<tr>';
+	echo '<th>&nbsp;</th>';
+	foreach ($particpants as $particpant) {
+		echo '<th>', $particpant, '</th>';
+	}
+	echo '</tr>', "\n";
+
+	// Best time for each day.
+	for ($day = 1; $day <= 25; $day++) {
+		echo '<tr>';
+		echo '<th>Day ', $day, '</th>';
+
+		foreach ($particpants as $particpant) {
+			echo '<td>';
+			if (isset($data['results'][$particpant]['days'][$day])) {
+				echo $data['results'][$particpant]['days'][$day][0];
+			} else {
+				echo '&nbsp;';
+			}
+			echo '</td>';
+		}
+		echo '</tr>', "\n";
+	}
+
+	echo '</table>';
+
+	echo '<h2>Hardware</h2>', "\n";
+	echo '<pre>';
+	echo $data['hardware'];
+	echo '</pre>';
+
+	echo '<h2>Raw Data</h2>', "\n";
+	echo '<pre>';
+	echo htmlspecialchars(json_encode($data['results'], JSON_PRETTY_PRINT));
+	echo '</pre>';
