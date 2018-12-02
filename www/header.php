da78f7b84fb324ca3ad502bb77cf8d30e6c891ac
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-02 23:22:26 +0000

    navbar brand should not take you away.

--- www/header.php
+++ www/header.php
@@ -24,7 +24,7 @@
       <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="/">AOC Benchmarks</a>
+      <a class="navbar-brand" href="#">AOC Benchmarks</a>
 
       <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar-nav mr-auto">
