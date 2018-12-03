96a2326ef26fdd0fb12892a30afb3f44d1d3a301
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-03 05:43:02 +0000

    Reset times before we run.

--- bench.php
+++ bench.php
@@ -2,7 +2,6 @@
 <?php
 	require_once(__DIR__ . '/config.php');
 
-	$lockfile = __DIR__ . '/.benchlock';
 	if (!file_exists($lockfile)) { file_put_contents($lockfile, ''); }
 	$fp = fopen($lockfile, 'r+');
 	if (!flock($fp, LOCK_EX | LOCK_NB)) {
@@ -147,6 +146,9 @@
 			// Run the day.
 			$long = false;
 			$hasRun = false;
+
+			// Reset the times.
+			$thisDay['times'] = [];
 
 			for ($i = 0; $i < ($long ? $longRepeatCount : $repeatCount); $i++) {
 				$start = time();
