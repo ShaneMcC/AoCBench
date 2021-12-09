fd92ffdc9f5fb7a91cb149eeb3448a52586fde99
Shane Mc Cormack <shanemcc@gmail.com>
2021-12-09 23:46:50 +0000

    Try to handle rabbitmq vanishing while we were waiting. Maybe.

--- checkScheduledUpdates.php
+++ checkScheduledUpdates.php
@@ -1,10 +1,13 @@
 <?php
 	require_once(__DIR__ . '/functions.php');
 
-	if (!$enableScheduledUpdates) { die('Scheduled updates not enabled for this instance.'); }
+	if (!$enableScheduledUpdates) { die('Scheduled updates not enabled for this instance.'."\n"); }
+
+	echo date('r'), ' - Started', "\n";
 
 	use PhpAmqpLib\Connection\AMQPStreamConnection;
 	use PhpAmqpLib\Message\AMQPMessage;
+	use PhpAmqpLib\Exception\AMQPTimeoutException;
 
 	$connection = new AMQPStreamConnection($rabbitmq['server'], $rabbitmq['port'], $rabbitmq['username'], $rabbitmq['password'], $rabbitmq['vhost']);
 	$channel = $connection->channel();
@@ -14,9 +17,14 @@
 	$channel->queue_bind($myQueue, 'events', '#');
 	$channel->basic_consume($myQueue, '', false, true, false, false, function ($msg) {
 		$m = json_decode($msg->body, true);
+		echo $msg->body, "\n";
 		handleScheduledUpdate($m['instance']);
 	});
 
-	while ($channel->is_open()) {
-	    $channel->wait();
+	while ($channel->is_consuming() && $channel->is_open()) {
+		try {
+			$channel->wait(null, false, 60);
+		} catch (AMQPTimeoutException $e) { }
 	}
+
+	echo date('r'), ' - Exited', "\n";
