dfb01d4b6801a62baf749df59885ccffb2559ff1
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-08 05:39:14 +0000

    Check the result of run.sh not of the POST variable assignemtn

--- checkRun.sh
+++ checkRun.sh
@@ -6,11 +6,12 @@ cd ${MYDIR}
 if [ -e ${MYDIR}/.doRun -a ! -e ${MYDIR}/.running ]; then
 	PRE=$(stat ${MYDIR}/.doRun)
 	${MYDIR}/run.sh
+	RES=$?
 	POST=$(stat ${MYDIR}/.doRun)
 
 	# Only delete file if run was successful, and it
 	# hasn't been re-touched since we started.
-	if [ $? -eq 0 -a "${PRE}" = "${POST}" ]; then
+	if [ $RES -eq 0 -a "${PRE}" = "${POST}" ]; then
 		rm ${MYDIR}/.doRun
 	fi;
 fi;
