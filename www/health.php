f3330ff540f8315b07d0ce1c2251cab26d99ef92
Shane Mc Cormack <shanemcc@gmail.com>
2023-12-11 01:49:17 +0000

    Hide some bits.

--- www/health.php
+++ www/health.php
@@ -20,7 +20,8 @@
             echo '<strong>Prepared:</strong> ', ($pdata['prepared'] ? 'true' : 'false'), '<br>';
             echo '<strong>Participant Type:</strong> ', ($pdata['participanttype'] == 2 ? 'yaml' : 'legacy'), '<br>';
             echo '<strong>Config:</strong>';
-            echo '<code><pre>';
+            echo '[<a href="#" data-toggle="collapse" data-target="#' . $person . '-code">show/hide</a>]';
+            echo '<code id="' . $person . '-code" class="collapse codeview"><pre>';
             echo spyc_dump($pdata['config']);
             echo '</pre></code>';
 
@@ -53,88 +54,111 @@
 
                 $rowspan = 1;
 
+                $dayClass = 'table-success';
+
                 ob_start();
-                echo '<tr class="', ($ddata['exists'] ? 'table-success' : 'table-danger'), '">';
+                echo '<tr>';
+                echo '<th>Log</th>';
+                echo '<td>', $ddata['log'], '</td>';
+                echo '</tr>';
+                $rowspan++;
+
+                echo '<tr class="collapse dayinfo ', ($ddata['exists'] ? 'table-success' : 'table-danger'), '">';
                 echo '<th>Day Exists</th>';
                 echo '<td>', ($ddata['exists'] ? 'true' : 'false'), '</td>';
                 echo '</tr>';
                 $rowspan++;
 
                 if ($ddata['exists']) {
-                    echo '<tr class="', ($ddata['wip'] ? 'table-danger' : 'table-success'), '">';
+                    echo '<tr class="collapse dayinfo ', ($ddata['wip'] ? 'table-warning' : 'table-success'), '">';
                     echo '<th>WIP</th>';
                     echo '<td>', ($ddata['wip'] ? 'true' : 'false'), '</td>';
                     echo '</tr>';
+                    if ($ddata['wip']) { $dayClass = 'table-warning'; }
                     $rowspan++;
 
-                    echo '<tr class="', ($ddata['ignored'] ? 'table-danger' : 'table-success'), '">';
+                    echo '<tr class="collapse dayinfo ', ($ddata['ignored'] ? 'table-warning' : 'table-success'), '">';
                     echo '<th>Ignored</th>';
                     echo '<td>', ($ddata['ignored'] ? 'true' : 'false'), '</td>';
                     echo '</tr>';
+                    if ($ddata['ignored']) { $dayClass = 'table-warning'; }
                     $rowspan++;
 
                     $bits = explode(':', $ddata['version']);
-                    echo '<tr>';
+                    echo '<tr class="collapse dayinfo">';
                     echo '<th>Common Version</th>';
                     echo '<td>', $bits[0], '</td>';
                     echo '</tr>';
                     $rowspan++;
 
-                    echo '<tr>';
+                    echo '<tr class="collapse dayinfo">';
                     echo '<th>Day Version</th>';
                     echo '<td>', $bits[1], '</td>';
                     echo '</tr>';
                     $rowspan++;
 
-                    echo '<tr>';
+                    echo '<tr class="collapse dayinfo">';
                     echo '<th>Input Version</th>';
                     echo '<td>', $ddata['input']['version'], '</td>';
                     echo '</tr>';
+                    if (empty($ddata['input']['version'])) { $dayClass = 'table-danger'; }
                     $rowspan++;
 
-                    echo '<tr>';
+                    echo '<tr class="collapse dayinfo">';
                     echo '<th>Answers Version</th>';
                     echo '<td>', $ddata['answers']['version'], '</td>';
                     echo '</tr>';
+                    if (empty($ddata['answers']['version'])) { $dayClass = 'table-danger'; }
                     $rowspan++;
 
-                    echo '<tr class="', ($ddata['answers']['part1'] ? 'table-success' : 'table-danger'), '">';
+                    echo '<tr class="collapse dayinfo ', ($ddata['answers']['part1'] ? 'table-success' : 'table-danger'), '">';
                     echo '<th>Has part 1 answer?</th>';
                     echo '<td>', ($ddata['answers']['part1'] ? 'true' : 'false'), '</td>';
                     echo '</tr>';
+                    if (!$ddata['answers']['part1']) { $dayClass = 'table-danger'; }
                     $rowspan++;
 
-                    echo '<tr class="', ($ddata['answers']['part2'] ? 'table-success' : 'table-danger'), '">';
+                    echo '<tr class="collapse dayinfo ', ($ddata['answers']['part2'] ? 'table-success' : 'table-danger'), '">';
                     echo '<th>Has part 2 answer?</th>';
                     echo '<td>', ($ddata['answers']['part2'] ? 'true' : 'false'), '</td>';
                     echo '</tr>';
+                    if (!$ddata['answers']['part2']) { $dayClass = 'table-danger'; }
                     $rowspan++;
 
-                    echo '<tr>';
+                    if (empty($ddata['runtype'])) { $runClass = 'table-success'; } // For now.
+                    else if ($ddata['runtype'] == 'hyperfine') { $runClass = 'table-success'; }
+                    else if ($ddata['runtype'] == 'time' && $pdata['participantversion'] != 1) {
+                        $runClass = 'table-warning';
+                    } else {
+                        $runClass = 'table-danger';
+                    }
+
+                    if ($runClass == 'table-warning' && $dayClass != 'table-danger') { $dayClass = 'table-warning'; }
+                    if ($runClass == 'table-danger') { $dayClass = 'table-danger'; }
+
+                    echo '<tr class="collapse dayinfo ', $runClass, '">';
                     echo '<th>Run Type</th>';
                     echo '<td>', $ddata['runtype'], '</td>';
                     echo '</tr>';
                     $rowspan++;
 
-                    echo '<tr>';
+                    if (!empty($ddata['length']) && $ddata['length'] != 'normal' && $dayClass != 'table-danger') { $dayClass = 'table-warning'; }
+                    echo '<tr class="collapse dayinfo ', (!empty($ddata['length']) && $ddata['length'] != 'normal' ? 'table-warning' : 'table-success'), '">';
                     echo '<th>Run Length</th>';
                     echo '<td>', $ddata['length'], '</td>';
                     echo '</tr>';
                     $rowspan++;
-
-                    echo '<tr>';
-                    echo '<th>Log</th>';
-                    echo '<td>', $ddata['log'], '</td>';
-                    echo '</tr>';
-                    $rowspan++;
+                } else {
+                    $dayClass = 'table-danger';
                 }
                 $dayRows = ob_get_clean();
 
-                $class = '';
-
-                echo '<tbody>';
+                echo '<tbody id="' . $person . '-day'.$day.'">';
                 echo '<tr>';
-                echo '<th rowspan=' . $rowspan. ' style="width: 50px" class="', $class, '">', $day, '</th>';
+                echo '<th rowspan=' . $rowspan. ' style="width: 200px" class="', $dayClass, '">';
+                echo $day;
+                echo '[<a href="#" data-toggle="collapse" data-target="#' . $person . '-day'.$day.' tr.dayinfo">show/hide</a>]';
+                echo '</th>';
                 echo '</tr>';
                 echo $dayRows;
                 echo '</tbody>';
