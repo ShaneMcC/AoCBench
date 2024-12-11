aa2a317a05cf6a88e812116611b19ed72a3e1135
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-11 22:14:32 +0000

    Add timestamping to run output.

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ SLUGNAME=$(basename "${PWD}")
 		touch ${MYDIR}/.running
 		rm /tmp/${SLUGNAME}.log
 
-		${MYDIR}/bench.php "${@}" | tee -a /tmp/${SLUGNAME}.log
-		${MYDIR}/inputMatrix.php --no-update "${@}" | tee -a /tmp/${SLUGNAME}.log
+		${MYDIR}/bench.php "${@}" | ts -m "%H:%M:%S |" | tee -a /tmp/${SLUGNAME}.log
+		${MYDIR}/inputMatrix.php --no-update "${@}" | ts -m "%H:%M:%S |" | tee -a /tmp/${SLUGNAME}.log
 
 		rm ${MYDIR}/.running
 		exit 0;
